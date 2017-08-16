6668dbc1ab9134bbdff26aef03b21ccf9f0c3d93
miguel angel <angel_7_dj@hotmail.com>
2017-08-16 08:21:02 -0500

    creamos una cuenta en git hub

--- index.php
+++ index.php
@@ -1,4 +1,85 @@
-
+<html>
+	<head>
+		<title>Programacion Visual</title>
+		<style type="text/css">
+			
+			* {
+				margin:0px;
+				padding:0px;
+			}
+			
+			#header {
+				margin:auto;
+				width:2000px;
+				font-family:Arial, Helvetica, sans-serif;
+			}
+			
+			ul, ol {
+				list-style:none;
+			}
+			
+			.nav > li {
+				float:left;
+			}
+			
+			.nav li a {
+				background-color:#000;
+				color:#fff;
+				text-decoration:none;
+				padding:10px 12px;
+				display:block;
+			}
+			
+			.nav li a:hover {
+				background="111"
+			}
+			
+			.nav li ul {
+				display:none;
+				position:absolute;
+				min-width:140px;
+			}
+			
+			.nav li:hover > ul {
+				display:block;
+			}
+			
+			.nav li ul li {
+				position:relative;
+			}
+			
+			.nav li ul li ul {
+				right:-140px;
+				top:0px;
+			}
+			
+		</style>
+	</head>
+	<body>
+	
+	<body background="fondo.jpg">
+		<div id="header">
+			<ul class="nav">
+			</ul>
+		</div>
+		<p>
+		         <br>
+		  
+				 <br>
+				 <font COLOR="red">
+				 <h1 ALIGN=center> UNIVERSIDAD AUTONOMA DEL ESTADO DE MEXICO </h1>
+				 <br>
+	             <h1 ALIGN=center> CENTRO UNIVERSITARIO UAEM ATLACOMULCO </h1>
+	             <br>
+	             <h1 ALIGN=center> INGENIERIA EN COMPUTACION </h1>
+				 <br>
+	             <h1 ALIGN=center> MIGUEL ANGEL LOPEZ MARTINEZ</h1>
+				 <br>
+	             <h1 ALIGN=center> LENGUAJE DE PROGRAMACION VISUAL </h1>
+				 </font>
+		</p>
+	</body>
+</html>
 
 
 
